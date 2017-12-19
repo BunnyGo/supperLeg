493e6b7c5ef0dde6e9d020d792c31c066410d895
siqi Fu <fusiqi3@163.com>
2017-12-20 00:03:24 +0800

    新增镜头脚本
    新增镜头脚本文件、新增场景测试元素

---
+++ client/SupperLeg/Assets/Script/cameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+    public GameObject target;
+    public float angle = 0;
+    public float pAngle = 0;
+    public float distance = 0;
+	// Use this for initialization
+	void Start () {
+        this.transform.position = target.transform.position;
+        this.transform.forward = target.transform.forward;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 camerPosition = new Vector3(0, distance * Mathf.Sin(pAngle*Mathf.PI/180), -distance * Mathf.Cos(pAngle*Mathf.PI/180));
+        this.transform.position = target.transform.position + camerPosition;
+        this.transform.eulerAngles = new Vector3(angle,0,0);
+	}
+}
